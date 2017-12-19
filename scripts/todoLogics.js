e811ecf310df0f2e3abeeb023e4d41bb12c5e2ca
j.majurel <j.majurel@bpp-tech.com>
2017-12-19 18:19:27 +0100

    added icon and todolist behaviour

--- scripts/todoLogics.js
+++ scripts/todoLogics.js
@@ -2,6 +2,7 @@ var todos = [];
 var toDoList = $("#todolist");
 var plus = $(".fa-plus"); 
 var input = $("input");
+var binIcon = '<i class="fa fa-trash-o" aria-hidden="true"></i>';
 
 input.keypress(function(ev) {
   if(ev.which === 13) {
@@ -17,11 +18,28 @@ input.keypress(function(ev) {
   }
 });
 
-plus.click( function() {
+plus.click(function() {
   input.slideToggle("slow");  
 });
 
 function updateToDoList(newToDo) {
-  toDoList.append("<li>" + newToDo + "</li>");
+  toDoList.append("<li>" + binIcon + newToDo + "</li>");
 };
 
+$("#todolist").on("click", "li", function(){
+  console.log("clicked");
+  $(this).toggleClass("done");
+});
+
+$("#todolist").on("mouseenter", "li", 
+  function() {
+    console.log("event fired");
+    $("i").fadeToggle("fast", "linear");
+  }, 
+);
+$("#todolist").on("mouseleave", "li",
+  function() {
+    console.log("mouse leave");
+    $("i").fadeToggle("fast", "linear");
+  }
+);
