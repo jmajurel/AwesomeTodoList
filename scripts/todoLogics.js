f1e836c3eaf3a740ea504a19de82e4c68bd5077c
j.majurel <j.majurel@bpp-tech.com>
2017-12-20 18:26:15 +0100

    added improvements in the li elements

--- scripts/todoLogics.js
+++ scripts/todoLogics.js
@@ -2,7 +2,7 @@ var todos = [];
 var toDoList = $("#todolist");
 var plus = $(".fa-plus"); 
 var input = $("input");
-var binIcon = '<i class="fa fa-trash-o" aria-hidden="true"></i>';
+var binIcon = '<i class="fa fa-trash-o fa-2x" aria-hidden="true"></i>';
 
 input.keypress(function(ev) {
   if(ev.which === 13) {
@@ -23,23 +23,15 @@ plus.click(function() {
 });
 
 function updateToDoList(newToDo) {
-  toDoList.append("<li>" + binIcon + newToDo + "</li>");
+  toDoList.append( "<li>" + binIcon + " " + newToDo + "</li>");
 };
 
 $("#todolist").on("click", "li", function(){
-  console.log("clicked");
   $(this).toggleClass("done");
 });
 
-$("#todolist").on("mouseenter", "li", 
+$("#todolist").on("mouseenter mouseleave", "li", 
   function() {
-    console.log("event fired");
-    $("i").fadeToggle("fast", "linear");
+    $(this).children("i").fadeToggle("medium", "linear");
   }, 
-);
-$("#todolist").on("mouseleave", "li",
-  function() {
-    console.log("mouse leave");
-    $("i").fadeToggle("fast", "linear");
-  }
 );
