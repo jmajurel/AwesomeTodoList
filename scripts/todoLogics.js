5dd01acfad469562e48c563d719a3c36663b44d8
j.majurel <j.majurel@bpp-tech.com>
2017-12-21 22:57:20 +0100

    changes in todolist

--- scripts/todoLogics.js
+++ scripts/todoLogics.js
@@ -22,16 +22,22 @@ plus.click(function() {
 });
 
 function updateToDoList(newToDo) {
-  var binIcon = '<i class="fa fa-trash-o fa-lg" aria-hidden="true"></i>';
-  toDoList.append(binIcon + newToDo);
+  var binIcon = '<button><i class="fa fa-trash-o fa-lg" aria-hidden="true"></i></button>';
+  toDoList.append('<div class="do">' + binIcon + "<span>" + newToDo + "</span>" + "</div>");
 };
 
 $("#todolist").on("click", "div", function(){
   $(this).toggleClass("done");
 });
 
-$("#todolist").on("mouseenter mouseleave", "div", 
+$("#todolist").on("mouseenter mouseleave", "div span", 
   function() {
-    $(this).children("i").fadeToggle("slow", "linear");
+    $(this).children("button").fadeToggle("slow", "linear");
   }, 
 );
+$("button").on("click", function(){
+  console.log("clicked");
+  //remove task from list
+  //todos.remove($(this).parent().text());
+  $(this).parent().remove();
+});
